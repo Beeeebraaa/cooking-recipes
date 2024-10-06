0b972bc0ea39b25faf8193a37c091a70b054285b
PashaStrayt <shulikovpand@gmail.com>
2024-10-07 02:24:05 +0300

    chore: app uppercase naming

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { render } from 'preact'
 
-import { App } from './app1'
+import { App } from './app'
 
 render(<App />, document.getElementById('app')!)
